42bc067981fc6483436c77f95f7c08bb97636768
Hamzat06 <59069709+Hamzat06@users.noreply.github.com>
2020-03-19 10:40:59 +0000

    Add files via upload

--- wbc/js/main.js
+++ wbc/js/main.js
@@ -35,8 +35,11 @@ $(document).ready(function(){
         $(".dropdown-content4").toggleClass("show");
     }, function(){
         $(".dropdown-content4").toggleClass("show").fadeOut();
-    })
+    });
 
+    $(".shape").on('click', function(){
+        $("#search").fadeToggle();
+    })
 
     $("#featured").owlCarousel({
         loop: true,
@@ -56,13 +59,15 @@ $(document).ready(function(){
             },
             600:{
                 items: 2,
-                margin: 5,
+                margin: 10,
+                loop: true,
                 nav: true
             },
             1000:{
                 items: 4,
                 nav: true,
-                loop: true
+                loop: true,
+                margin: 10
             }
         }
 
@@ -93,6 +98,38 @@ $(document).ready(function(){
             1000:{
                 items: 1,
                 nav: true,
+                loop: true
+            }
+        }
+    });
+
+    $("#featured-destination").owlCarousel({
+        loop: true,
+        margin: 10,
+        navContainer: '#customNav3',
+        dots: true,
+        navText: [
+        '<i class="fa fa-chevron-left fa-3x" aria-hidden="true"></i>',
+        '<i class="fa fa-chevron-right fa-3x" aria-hidden="true"></i>'
+        ],
+        navContainer: '#destinations #customNav3',
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items: 1,
+                loop: true,
+                nav: true
+            },
+            600:{
+                items: 3,
+                loop: true,
+                margin: 10,
+                nav: true
+            },
+            1000:{
+                items: 3,
+                nav: true,
+                margin: 10,
                 loop: true
             }
         }
